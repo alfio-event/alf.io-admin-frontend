5bcdae8f1bac12ecdd23d5021d94bef5b8694263
Sylvain Jermini <sylvain.jermini@syjer.com>
2019-11-25 14:25:31 +0100

    initial stubs/import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClientXsrfModule, HttpClient } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { CustomLoader } from './shared/i18n.service';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new CustomLoader(http);
+}
+
 
 @NgModule({
   declarations: [
@@ -12,7 +23,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    HttpClientModule,
+    HttpClientXsrfModule.withOptions({
+      cookieName: 'XSRF-TOKEN',
+      headerName: 'X-CSRF-TOKEN',
+    }),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/i18n.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { TranslateLoader } from '@ngx-translate/core';
+import { Observable } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+
+const translationCache: {[key: string]: Observable<any>} = {};
+
+
+@Injectable({providedIn: 'root'})
+export class CustomLoader implements TranslateLoader {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getTranslation(lang: string): Observable<any> {
+    if (!translationCache[lang]) {
+      translationCache[lang] = this.http.get(`/api/v2/admin/i18n/bundle/${lang}`).pipe(shareReplay(1));
+    }
+    return translationCache[lang];
+  }
+}
