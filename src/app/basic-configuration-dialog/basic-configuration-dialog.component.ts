591c992b3891baef3d483203ec6675615f97d950
Sylvain Jermini <sylvain.jermini@syjer.com>
2019-12-06 15:57:42 +0100

    more mapping

--- src/app/basic-configuration-dialog/basic-configuration-dialog.component.ts
+++ src/app/basic-configuration-dialog/basic-configuration-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ConfigurationService } from '../shared/configuration.service';
+import { ConfigurationService, ConfigurationMap } from '../shared/configuration.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Language } from '../model/language';
 import { forkJoin } from 'rxjs';
@@ -13,15 +13,19 @@ export class BasicConfigurationDialogComponent implements OnInit {
 
   basicConfiguration: FormGroup;
   allLanguages: Language[] = [];
+  allConf: ConfigurationMap;
 
   constructor(private configurationService: ConfigurationService, fb: FormBuilder) {
     forkJoin(configurationService.getAllLanguages(), configurationService.loadAllSystemLevel()).subscribe(([languages, allConf]) => {
       this.allLanguages = languages;
+      this.allConf = allConf;
       const currentLang = parseInt(allConf.GENERAL.SUPPORTED_LANGUAGES.value);
       let selectedLanguages: number[] = [];
       if (!isNaN(currentLang)) {
         selectedLanguages = configurationService.mapLanguagesConfigurationValueToLanguages(currentLang, languages).map(l => l.value);
       }
+
+      console.log(allConf);
 
       this.basicConfiguration = fb.group({
         selectedLanguages: [selectedLanguages],
@@ -30,7 +34,10 @@ export class BasicConfigurationDialogComponent implements OnInit {
           type: allConf.MAIL.MAILER_TYPE.value
         }),
         map: fb.group({
-          provider: allConf.MAP.MAPS_PROVIDER.value
+          provider: allConf.MAP.MAPS_PROVIDER.value,
+          MAPS_CLIENT_API_KEY: allConf.MAP.MAPS_CLIENT_API_KEY.value,
+          MAPS_HERE_APP_ID: allConf.MAP.MAPS_HERE_APP_ID.value,
+          MAPS_HERE_APP_CODE: allConf.MAP.MAPS_HERE_APP_CODE.value
         })
       });
     });
--- src/app/shared/configuration.service.ts
+++ src/app/shared/configuration.service.ts
@@ -5,6 +5,8 @@ import { Language } from '../model/language';
 import { ConfigurationKeyValue, SettingCategory, ConfigurationKey } from '../model/configuration';
 import { map } from 'rxjs/operators';
 
+export type ConfigurationMap = {[key in SettingCategory]?: {[k2 in ConfigurationKey]? : ConfigurationKeyValue}};
+
 @Injectable({providedIn: 'root'})
 export class ConfigurationService {
 
@@ -18,7 +20,7 @@ export class ConfigurationService {
         return this.http.get<boolean>('/admin/api/configuration/basic-configuration-needed');
     }
 
-    loadAllSystemLevel(): Observable<{[key in SettingCategory]?: {[k2 in ConfigurationKey]? : ConfigurationKeyValue}}> {
+    loadAllSystemLevel(): Observable<ConfigurationMap> {
         return this.http.get<{[key in SettingCategory]?: ConfigurationKeyValue[]}>('/admin/api/configuration/load').pipe(map(transformConfRes));
     }
 
@@ -34,7 +36,7 @@ export class ConfigurationService {
 }
 
 // transform a Map<SettingCategory, ConfigurationKeyValue[]> in Map<SettingCategory, Map<SettingCategory, ConfigurationKeyValue>>
-function transformConfRes(res: {[key in SettingCategory]?: ConfigurationKeyValue[]}) : {[key in SettingCategory]?: {[k2 in ConfigurationKey]? : ConfigurationKeyValue}} {
+function transformConfRes(res: {[key in SettingCategory]?: ConfigurationKeyValue[]}) : ConfigurationMap {
     let confMap = {};
     Object.keys(res).forEach((k) => {
         let confKeyValues: ConfigurationKeyValue[] = res[k];
