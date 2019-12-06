a47b40ef9ee5f238b7ddea59ad253bca9f23cccc
Sylvain Jermini <sylvain.jermini@syjer.com>
2019-12-06 15:04:09 +0100

    transform configuration return type

--- src/app/shared/configuration.service.ts
+++ src/app/shared/configuration.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Language } from '../model/language';
-import { ConfigurationKeyValue, SettingCategory } from '../model/configuration';
+import { ConfigurationKeyValue, SettingCategory, ConfigurationKey } from '../model/configuration';
+import { map } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class ConfigurationService {
@@ -17,7 +18,22 @@ export class ConfigurationService {
         return this.http.get<boolean>('/admin/api/configuration/basic-configuration-needed');
     }
 
-    loadAllSystemLevel(): Observable<Record<SettingCategory, ConfigurationKeyValue>> {
-        return this.http.get<Record<SettingCategory, ConfigurationKeyValue>>('/admin/api/configuration/load');
+    loadAllSystemLevel(): Observable<{[key in SettingCategory]?: {[k2 in ConfigurationKey]? : ConfigurationKeyValue}}> {
+        return this.http.get<{[key in SettingCategory]?: ConfigurationKeyValue[]}>('/admin/api/configuration/load').pipe(map(transformConfRes));
     }
+}
+
+// transform a Map<SettingCategory, ConfigurationKeyValue[]> in Map<SettingCategory, Map<SettingCategory, ConfigurationKeyValue>>
+function transformConfRes(res: {[key in SettingCategory]?: ConfigurationKeyValue[]}) : {[key in SettingCategory]?: {[k2 in ConfigurationKey]? : ConfigurationKeyValue}} {
+    let confMap = {};
+    Object.keys(res).forEach((k) => {
+        let confKeyValues: ConfigurationKeyValue[] = res[k];
+        confKeyValues.forEach((v) => {
+            if(!confMap[k]) {
+                confMap[k] = {};
+            }
+            confMap[k][v.key] = v;
+        });
+    })
+    return confMap;
 }
