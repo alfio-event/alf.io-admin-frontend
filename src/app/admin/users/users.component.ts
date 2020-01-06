60b9c8de6ac6e3cb745df996a713a274adc7184f
Sylvain Jermini <sylvain.jermini@syjer.com>
2020-01-06 20:15:51 +0100

    add support for user deletion

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/shared/user.service';
 import { User, Role, RoleDescriptor, UserType } from 'src/app/model/user';
 import { Observable } from 'rxjs';
+import { MatDialog } from '@angular/material';
+import { ConfirmDialogComponent } from 'src/app/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-users',
@@ -10,17 +12,32 @@ import { Observable } from 'rxjs';
 export class UsersComponent implements OnInit {
 
   users: User[] = [];
-  usersDisplayColumns = ['enabled', 'username', 'name', 'organization', 'role'];
+  usersDisplayColumns = ['enabled', 'username', 'name', 'organization', 'role', 'actions'];
 
   rolesDescriptor$: Observable<{ [key in Role]?: RoleDescriptor }>
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private dialog: MatDialog) { }
 
   ngOnInit() {
+    this.loadUsers();
+    this.rolesDescriptor$ = this.userService.getRolesDescriptor();
+  }
+
+  private loadUsers() {
     this.userService.getAllUsers().subscribe(users => {
       this.users = users.filter(u => u.type !== UserType.API_KEY);
     });
-    this.rolesDescriptor$ = this.userService.getRolesDescriptor();
+  }
+
+  deleteUser(user: User) {
+    let msg = 'The user ' + user.username + ' will be deleted. Are you sure?';
+    this.dialog.open(ConfirmDialogComponent, {width: '400px', data: {title: 'Confirm deletion', message: msg}}).afterClosed().subscribe(res => {
+      if (res) {
+        this.userService.deleteUser(user).subscribe(res => {
+          this.loadUsers();
+        })
+      }
+    });
   }
 
 }
