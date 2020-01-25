272fd42172462b760259421a7b4f1ccdec03e66d
Sylvain Jermini <sylvain.jermini@syjer.com>
2020-01-25 17:16:07 +0100

    extension log: add pagination

--- src/app/admin/extension/extension-log/extension-log.component.ts
+++ src/app/admin/extension/extension-log/extension-log.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ExtensionService } from 'src/app/shared/extension.service';
+import { MatPaginator } from '@angular/material';
+import { ExtensionLog } from 'src/app/model/extension-log';
+import { DataSource } from '@angular/cdk/table';
+import { CollectionViewer } from '@angular/cdk/collections';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-extension-log',
@@ -7,18 +12,45 @@ import { ExtensionService } from 'src/app/shared/extension.service';
 })
 export class ExtensionLogComponent implements OnInit {
 
-  extensionLogs = [];
+  extensionLogs: ExtensionLogDataSource;
   count = 0;
 
-  extensionLogsDisplayColumns = ['path', 'name', 'effectivePath', 'type', 'description', 'date']
+  extensionLogsDisplayColumns = ['path', 'name', 'effectivePath', 'type', 'description', 'date'];
+
+  @ViewChild(MatPaginator, { static: true })
+  paginator: MatPaginator;
 
   constructor(private extensionService: ExtensionService) { }
 
   ngOnInit() {
-    this.extensionService.getLog(0).subscribe(paginated => {
-      this.extensionLogs = paginated.left;
-      this.count = paginated.right;
-    });
+    this.extensionLogs = new ExtensionLogDataSource(this.extensionService, this.paginator);
+    this.extensionLogs.loadExtensionLog(0)
   }
 
+  loadPage() {
+    this.extensionLogs.loadExtensionLog(this.paginator.pageIndex);
+  }
+}
+
+//based on https://blog.angular-university.io/angular-material-data-table/
+class ExtensionLogDataSource implements DataSource<ExtensionLog> {
+
+  private extensionLogSubject = new BehaviorSubject<ExtensionLog[]>([]);
+
+  constructor(private extensionService: ExtensionService, private paginator: MatPaginator) {}
+
+  connect(collectionViewer: CollectionViewer): Observable<ExtensionLog[]> {
+    return this.extensionLogSubject.asObservable();
+  }
+
+  disconnect(collectionViewer: CollectionViewer): void {
+    this.extensionLogSubject.complete();
+  }
+
+  loadExtensionLog(pageIndex: number = 0) {
+    this.extensionService.getLog(pageIndex).subscribe(log => {
+      this.paginator.length = log.right; //total size
+      this.extensionLogSubject.next(log.left)
+    });
+  }
 }
