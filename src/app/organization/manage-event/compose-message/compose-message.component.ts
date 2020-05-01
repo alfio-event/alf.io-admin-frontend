29ee037013daf7f5c1c3b0f1acb61d30c2e09477
Sylvain Jermini <sylvain.jermini@syjer.com>
2020-05-01 15:58:28 +0200

    compose message: build form array

--- src/app/organization/manage-event/compose-message/compose-message.component.ts
+++ src/app/organization/manage-event/compose-message/compose-message.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from 'src/app/shared/event.service';
 import { ActivatedRoute } from '@angular/router';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
+import { Language } from 'src/app/model/language';
 
 @Component({
   selector: 'app-compose-message',
@@ -11,21 +12,31 @@ export class ComposeMessageComponent implements OnInit {
 
   eventShortName: string;
 
-  composeMessageForm: FormGroup;
+  composeMessageForm: FormArray
+  languages: Language[] = [];
 
   constructor(private eventService: EventService, private route: ActivatedRoute, private fb: FormBuilder) { }
 
   ngOnInit(): void {
 
-    this.composeMessageForm = this.fb.group({});
+    this.composeMessageForm = this.fb.array([]);
 
     this.eventShortName = this.route.parent.snapshot.params['eventShortName'];
     this.eventService.getSelectedLanguages(this.eventShortName).subscribe(res => {
-      console.log(res);
+      this.languages = res;
+      res.forEach((v, idx) => {
+        this.composeMessageForm.insert(idx, this.fb.group({
+          attachTicket: false,
+          locale: v.locale,
+          subject: null,
+          text: null
+        }));
+      });
     });
   }
 
   sendMessage(): void {
+    console.log(this.composeMessageForm.value);
   }
 
 }
